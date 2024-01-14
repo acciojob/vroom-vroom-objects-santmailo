6aed3a88861c6231cdb23356d8448406995142e6
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2024-01-14 22:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,32 @@
 function Car(make, model) {
 	this.make = make;
 	this.model = model;
-	getMakeModel: function () {
-		return `${this.make} ${this.model}`
-	}
 }
 
-function SportsCar extends Car (make, model, topSpeed) {
-	super(make,model);
-	this.topSpeed = topSpeed;
-	getTopSpeed: function() {
-		return `${this.topSpeed}`
+Car.prototype.getMakeModel = function () {
+		return this.make+ " " + this.model;
 	}
+
+
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
 }
+
+
+// Inheriting the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Resetting the constructor property for SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Adding a method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
+
+
 
 // Do not change the code below
 window.Car = Car;
