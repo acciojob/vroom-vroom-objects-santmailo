b7e695c01704d2d1ae970c4ed9b9c147daa59753
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2024-01-14 22:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+	getMakeModel: function () {
+		return `${this.make} ${this.model}`
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar extends Car (make, model, topSpeed) {
+	super(make,model);
+	this.topSpeed = topSpeed;
+	getTopSpeed: function() {
+		return `${this.topSpeed}`
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
